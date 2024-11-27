9c7cebb50072c8fa1a939709080778878ee790a0
JoseM0lina <josmmoli@espol.edu.ec>
2024-11-27 09:29:26 -0500

    Estructura básica: IndicatorWeather

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import Grid from '@mui/material/Grid2'
 import './App.css'
+import IndicatorWeather from './components/IndicatorWeather';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1<IndicatorWeather /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2<IndicatorWeather /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3<IndicatorWeather /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4<IndicatorWeather /></Grid>
     
       {/* Tabla */}
       <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
